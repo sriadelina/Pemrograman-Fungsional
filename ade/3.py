007c6b60153b9c735edf491c0b22fb98bdf55466
sriadelina <104374982+sriadelina@users.noreply.github.com>
2022-09-20 20:15:06 +0700

    Delete 3.py

--- ade/3.py
+++
@@ -1,9 +0,0 @@
-def pangkat(bilangan,pangkat):
-  if (bilangan < 0):
-    return print("Hanya Masukkan bilangan positif")
-  elif (type(bilangan) != int):
-    return print("Hanya Masukkan bilangan bulat")
-
-  return (bilangan**pangkat)
-  
-pangkat(3,2)
